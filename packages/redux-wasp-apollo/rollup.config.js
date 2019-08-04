9c859862c5ef49fdb0bd0fc98180627e5d5365e5
Reynolds A. Colon <reynolds@Reynoldss-MBP.home>
2019-08-04 19:06:08 -0400

    refactor redux-wasp-apollo to include updated repository

--- packages/redux-wasp-apollo/rollup.config.js
+++ packages/redux-wasp-apollo/rollup.config.js
@@ -1,7 +1,10 @@
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
   input: './index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins: [resolve()]
 }
